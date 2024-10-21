bb3ef50b5a7a1594d59832bf528fddb2de092929
Natchalit Namkong <nickchickydoo@MacBook-Pro-khxngNatchalit.local>
2024-10-21 17:30:32 +0700

    save cidi

--- services/rtx/route.go
+++ services/rtx/route.go
@@ -37,10 +37,11 @@ func (r *ROUTEX) Connect(rx func(*gin.Engine, *gin.RouterGroup)) {
 	router.Use(gin.Recovery())
 
 	addr := ``
-	if os.Getenv(`APP_ENV`) == `develop` {
+	appEnv := os.Getenv(`APP_ENV`)
+	if appEnv == `develop` {
 		addr = fmt.Sprintf("127.0.0.1:%d", r.PORT)
 	} else {
-		addr = fmt.Sprintf(":%d", r.PORT)
+		addr = fmt.Sprintf("0.0.0.0:%d", r.PORT)
 	}
 
 	s := &http.Server{
